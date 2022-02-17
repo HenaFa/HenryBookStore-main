72bba87ab8fc8c35da0de6fbeafb9cb767ea2877
HenaFa <henry.fabritius@myy.haaga-helia.fi>
2022-02-17 12:51:26 +0200

    muutos

--- src/main/java/com/example/HenryBookstrore/web/BookController.java
+++ src/main/java/com/example/HenryBookstrore/web/BookController.java
@@ -14,31 +14,31 @@ import com.example.HenryBookstrore.domain.BookRepository;
 public class BookController {
 	@Autowired
 	private BookRepository repository;
-
+//listaa
 	@RequestMapping(value = { "/", "/booklist" })
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
-
+//lisää kirjan
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		return "addbook";
 	}
-
+//tallentaa
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
-
+//delete
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
-
+//edittaus
 	@RequestMapping("/edit/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
